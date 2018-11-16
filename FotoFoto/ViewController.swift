77e0dcce99b01d6bece66d0b418d86e352caf2f3
dgwu <danielgunawanwu@gmail.com>
2018-11-16 09:44:24 +0700

    Initial Commit

---
+++ FotoFoto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FotoFoto
+//
+//  Created by Daniel Gunawan on 16/11/18.
+//  Copyright © 2018 Daniel Gunawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
