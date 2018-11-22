7daaea240d9953473c53a277070ecc24efa42d18
okky pribadi <donbosco_okky@yahoo.com>
2018-11-22 09:49:55 +0700

    tes push

---
+++ FotoFoto/PhotoGrid/View/Camera/CaptureCameraVC.swift
@@ -0,0 +1,19 @@
+//
+//  CaptureCameraVC.swift
+//  FotoFoto
+//
+//  Created by okky pribadi on 16/11/18.
+//  Copyright © 2018 Daniel Gunawan. All rights reserved.
+//
+
+import UIKit
+
+class CaptureCameraVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
